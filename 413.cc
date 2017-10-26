0b41a5efcc8da3f3df6e5c9983227c899389eb32
Pumpkin <pumpkin@Pumpkins-Mac-mini.local>
2017-10-26 20:02:06 +0800

    optimise 413

--- 413.cc
+++ 413.cc
@@ -1,37 +1,23 @@
-#include <cmath>
 #include <vector>
-#include <iostream>
 
 
 using namespace std;
 
-
 class Solution {
 public:
-	int numberOfCombination(int size) {
-		return (size - 1) * (size - 2) / 2;
-	}
-
 	int numberOfArithmeticSlices(vector<int>& A) {
 		if (A.size() < 3) return 0;
 
 		auto result = 0;
-
-		auto size = 2;
-		auto diff = A[1] - A[0];
+		auto current = 0;
 		for (auto n = 1; n < A.size() - 1; ++n) {
-			if (A[n + 1] - A[n] == diff) {
-				size += 1;
+			if (A[n] - A[n - 1] == A[n + 1] - A[n]) {
+				current += 1;
+				result += current;
 			} else {
-				if (size >= 3) result += numberOfCombination(size);
-
-				size = 2;
-				diff = A[n + 1] - A[n];
+				current = 0;
 			}
 		}
-
-		if (size >= 3) result += numberOfCombination(size);
-
 		return result;
 	}
 };
