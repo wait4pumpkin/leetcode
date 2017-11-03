bf104f6590fa226890804404e9865d5f6f02e3fd
wait4pumpkin <wait4pumpkin@gmail.com>
2017-11-03 10:36:28 +0800

    optimize 491

--- 491.cc
+++ 491.cc
@@ -1,45 +1,24 @@
 class Solution {
 public:
 	vector<vector<int>> findSubsequences(vector<int>& nums) {
-		vector<Node> memo(nums.size());
-		for (auto n = 0; n < nums.size(); ++n) {
-			auto &node = memo[n];
-			node.value = nums[n];
-
-			for (auto next = n + 1; next < nums.size(); ++next) {
-				if (nums[next] >= nums[n]) {
-					node.children.push_back(next);
-				}
-			}
-		}
-
-		set<vector<int>> paths;
-		for (auto n = 0; n < nums.size(); ++n) {
-			stack<pair<int, vector<int>>> pending;
-			pending.push(make_pair(n, vector<int>()));
-			while (!pending.empty()) {
-				auto content = pending.top();
-				pending.pop();
-
-				auto &node = memo[content.first];
-				auto &path = content.second;
-				path.push_back(node.value);
-				if (path.size() >= 2) paths.insert(path);
-
-				for (auto child : node.children) {
-					pending.push(make_pair(child, path));
-				}
-			}
-		}
-
+		vector<int> path;
 		vector<vector<int>> result;
-		result.insert(result.end(), paths.begin(), paths.end());
+		findSubsequences(nums, 0, path, result);
 		return result;
 	}
 
 private:
-	struct Node {
-		int value;
-		vector<int> children;
-	};
+	void findSubsequences(vector<int> &nums, const int start, vector<int> &path, vector<vector<int>> &result) {
+		if (path.size() > 1) result.push_back(path);
+
+		unordered_set<int> visited;
+		for (auto n = start; n < nums.size(); ++n) {
+			if ((path.empty() || nums[n] >= path.back()) && visited.find(nums[n]) == visited.end()) {
+				visited.insert(nums[n]);
+				path.push_back(nums[n]);
+				findSubsequences(nums, n + 1, path, result);
+				path.pop_back();
+			}
+		}
+	}
 };
